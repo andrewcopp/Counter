b4ac8975c759fccb356576d98712814cf980d1f9
Andrew Copp <andrew.w.copp@gmail.com>
2017-11-29 18:51:37 -0500

    Adding CountViewController

---
+++ Counter/Counter/CountViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CountViewController.swift
+//  Counter
+//
+//  Created by Andrew Copp on 11/29/17.
+//  Copyright © 2017 Andrew Copp. All rights reserved.
+//
+
+import UIKit
+
+class CountViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
